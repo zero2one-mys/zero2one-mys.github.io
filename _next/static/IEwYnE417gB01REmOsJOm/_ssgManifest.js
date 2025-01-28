5aa1fd4675fa572686d567e4b336228812fb842f
fujiser <fujiser@users.noreply.github.com>
2025-01-28 08:40:38 +0000

    deploy: 34ed888fee6f1d19da5873540d80449a63899682

---
+++ _next/static/IEwYnE417gB01REmOsJOm/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[app]","\u002F[app]\u002Feula","\u002F[app]\u002Fprivacy-policy","\u002F[app]\u002Fterms-and-conditions"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/(top)/layout-630fbd5818427fd8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95,131,156,161,307,326,372,487,916,952],{6072:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,441,517,358],()=>s(6072)),_N_E=e.O()}]);
